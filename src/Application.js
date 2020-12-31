336054b026c7a7f4115177725c86abf4cac8ee52
Mariusz <56251640+mszgajewski@users.noreply.github.com>
2020-12-31 11:18:54 +0100

    Add files via upload

---
+++ src/Application.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import HighScore from './HighScore';
+import './css/style.css';
+
+class Application extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            count: 0,
+            overTen: false
+        }
+    }
+   handleClick = () => {
+        this.setState({count: this.state.count + 1});
+   }
+
+   componentDidUpdate(props, state){
+       if(this.state.count > 10 && this.state.count != state.count && !this.state.overTen){
+           console.log("updating over ten");
+           this.setState({overTen: true});
+       }
+   }
+
+   resetCount =(e) => {
+       console.log("Event is", e);
+       this.setState({
+           count: 0,
+           overTen: false
+       });
+   }
+    
+    render(){
+        let {count} = this.state;
+       
+        return (
+            <div>
+                <h1>Kliknąłeś przycisk {count} razy</h1>
+               <HighScore
+                    overTen={this.state.overTen}
+                    onReset={this.resetCount}
+               />
+                <span>
+                    <button onClick={() => this.handleClick()}>Kliknij mnie</button>
+                </span>
+            </div>
+        );
+    }
+}
+
+export default Application;
